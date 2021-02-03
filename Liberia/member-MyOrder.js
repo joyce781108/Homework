162453397a045b094011cc547102355d02848194
joyce <joyce781108@gmail.com>
2021-02-04 00:26:33 +0800

    修改

---
+++ Liberia/member-MyOrder.js
@@ -0,0 +1,32 @@
+
+$('#inquire').click(function(){ 
+    
+    $('.search__list').slideToggle('slow');
+});
+
+$('.search__list li').click(function(){
+    $('.search__title p').html( $(this).text());
+    if($(this).text() == "六個月內訂單(含出貨後)"){
+        $('.content__title em').text("所有訂單")
+    }
+    else{
+        $('.content__title em').html( $(this).text());
+    }
+    $('.search__list').slideToggle('hide');
+});
+
+$('.order__inquire label').click(function(){
+    if($(this).text() == "六個月內訂單(含出貨後)"){
+        $('.content__title em').text("所有訂單")
+    }
+    else if($(this).attr('for') == "numbering"){
+        $('.content__title em').text("訂單查詢")
+    }
+    else{
+        $('.content__title em').html( $(this).text());
+    }
+    
+});
+$('#transa-ctionid').click(function(){
+    
+})
