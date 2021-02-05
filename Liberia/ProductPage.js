e1e8b7399e34bfab5ceb1896541aa402ca118110
joyce <joyce781108@gmail.com>
2021-02-05 17:14:23 +0800

    修改

--- Liberia/ProductPage.js
+++ Liberia/ProductPage.js
@@ -1,7 +1,14 @@
 $('#sort').click(function(){
     $('.sort__list').slideToggle('slow');
 });
+$('.sort__list li').click(function(){
+    $('.sort__list').slideToggle('hled');
+});
 
 $('#class').click(function(){
     $('.class__list').slideToggle('slow');
+});
+
+$('.class__list li').click(function(){
+    $('.class__list').slideToggle('hled');
 });
