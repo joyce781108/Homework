12783fe01ac9bdf9fd7d32c846cf0da70686fb62
joyce <joyce781108@gmail.com>
2021-02-05 10:56:29 +0800

    完成

--- Liberia/member-MyOrder.js
+++ Liberia/member-MyOrder.js
@@ -4,6 +4,7 @@ $('#inquire').click(function(){
     $('.search__list').slideToggle('slow');
 });
 
+
 $('.search__list li').click(function(){
     $('.search__title p').html( $(this).text());
     if($(this).text() == "六個月內訂單(含出貨後)"){
@@ -15,15 +16,24 @@ $('.search__list li').click(function(){
     $('.search__list').slideToggle('hide');
 });
 
+
 $('.order__inquire label').click(function(){
     if($(this).text() == "六個月內訂單(含出貨後)"){
         $('.content__title em').text("所有訂單")
+        
+    }
+    else if($(this).html() == "&nbsp;"){
+        $('.content__title em').text("訂單查詢")
+        
+
     }
     else{
         $('.content__title em').html( $(this).text());
     }
-    
 });
-$('#transa-ctionid').click(function(){
+
+
+$('input[name=transa-ctionid]:text').click(function(){
     $('.content__title em').text("訂單查詢")
+    document.getElementById("transa-ctionid_radio").click();
 })
